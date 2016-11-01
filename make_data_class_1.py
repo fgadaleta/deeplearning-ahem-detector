7e2edbdbdee95cacc85cd9cbc0c97b1dd44b472f
Francesco Gadaleta <fgadalet@its.jnj.com>
2016-11-01 13:48:40 -0400

    final

--- make_data_class_1.py
+++ make_data_class_1.py
@@ -1,3 +1,25 @@
+"""
+MIT License
+Copyright (c) 2016 Francesco Gadaleta 
+Permission is hereby granted, free of charge, to any person obtaining a copy 
+of this software and associated documentation files (the "Software"), to deal 
+in the Software without restriction, including without limitation the rights 
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+of the Software, and to permit persons to whom the Software is furnished to do 
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+""" 
+
 import matplotlib
 # Force matplotlib to not use any Xwindows backend.
 matplotlib.use('Agg')
@@ -32,8 +54,5 @@ for x in xrange(0, numsamples-windowsize, step):
     ut.printStuff('Creating spectrum image class_1 samples [%d-%d] of %d file %d',(b,e, numsamples, numfiles))
     filename = os.path.join(path, 'class_1/partial_spectrum_%d.png'%x)
     ut.specgram_frombuffer(audiosamples[b:e], dimx, dimy, fname=filename, dpi=180)
-    #if x == maxfiles:
-    #    break
- 
         
 print('\nbye!\n')        
