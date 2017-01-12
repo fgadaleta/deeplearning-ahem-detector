aa8c33c171189eeda7599e6897c56489a714e4f1
SidneyW <sidneywijngaarde@gmail.com>
2017-01-11 21:31:34 -0500

    Attempting to load the model

--- ahem_detector.py
+++ ahem_detector.py
@@ -30,8 +30,8 @@ Please build training/testing set before running this script.
 Make sure to create the local path that have been hardcoded in the following scripts,
 then execute
 
-    % python make_data_class_0.py
-    % python make_data_class_1.py
+	% python make_data_class_0.py
+	% python make_data_class_1.py
 ---------------------------------------------------------------------------------------
 
 """
@@ -40,21 +40,23 @@ from __future__ import print_function
 import numpy as np
 
 import matplotlib
+matplotlib.use('Agg')
 # Force matplotlib to not use any Xwindows backend.
-# import matplotlib.pyplot as plt
-# import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import matplotlib.style as ms
+ms.use('seaborn-muted')
 # get_ipython().magic('matplotlib inline')
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
-# from keras import backend as K
-# from keras.models import model_from_json
+from keras import backend as K
+from keras.models import model_from_json
 
 import skimage.io as io
-# from skimage.measure import block_reduce
+from skimage.measure import block_reduce
 
 import os
 from os import listdir
@@ -66,9 +68,7 @@ import librosa.display
 import IPython.display
 
 
-ms.use('seaborn-muted')
-matplotlib.use('Agg')
-np.random.seed(1337)  # for reproducibility
+# np.random.seed(1337)  # for reproducibility
 # In[2]:
 
 # network configuration
@@ -96,6 +96,7 @@ path_class_1 = './archive/ahem_data/class_1/'
 class0_files = [f for f in listdir(path_class_0) if isfile(join(path_class_0, f))]
 class1_files = [f for f in listdir(path_class_1) if isfile(join(path_class_1, f))]
 
+print(len(class0_files), len(class1_files))
 
 # In[4]:
 
@@ -103,19 +104,19 @@ class1_files = [f for f in listdir(path_class_1) if isfile(join(path_class_1, f)
 X_t = []
 Y_t = []
 
-for fn in class0_files[:100]:
-    img = io.imread(os.path.join(path_class_0, fn))
-    img = img.transpose((2, 0, 1))
-    img = img[:3, :, :]
-    X_t.append(img)
-    Y_t.append(0)
+for fn in class0_files[:400]:
+	img = io.imread(os.path.join(path_class_0, fn))
+	img = img.transpose((2, 0, 1))
+	img = img[:3, :, :]
+	X_t.append(img)
+	Y_t.append(0)
 
-for fn in class1_files[:100]:
-    img = io.imread(os.path.join(path_class_1, fn))
-    img = img.transpose((2, 0, 1))
-    img = img[:3, :, :]
-    X_t.append(img)
-    Y_t.append(1)
+for fn in class1_files[:40]:
+	img = io.imread(os.path.join(path_class_1, fn))
+	img = img.transpose((2, 0, 1))
+	img = img[:3, :, :]
+	X_t.append(img)
+	Y_t.append(1)
 
 X_t = np.asarray(X_t)
 X_t = X_t.astype('float32')
@@ -129,7 +130,7 @@ Y_t = np_utils.to_categorical(Y_t, nb_classes)
 
 img_rows, img_cols = X_t.shape[2], X_t.shape[3]
 # input image dimensions
-img_channels = 3               # RGB
+img_channels = 3			   # RGB
 input_shape = (3, img_rows, img_cols)
 
 
@@ -139,19 +140,19 @@ input_shape = (3, img_rows, img_cols)
 X_test = []
 Y_test = []
 
-for fn in class0_files[6000:8000]:
-    img = io.imread(os.path.join(path_class_0, fn))
-    img = img.transpose((2, 0, 1))
-    img = img[:3, :, :]
-    X_test.append(img)
-    Y_test.append(0)
+for fn in class0_files[400:len(class0_files)]:
+	img = io.imread(os.path.join(path_class_0, fn))
+	img = img.transpose((2, 0, 1))
+	img = img[:3, :, :]
+	X_test.append(img)
+	Y_test.append(0)
 
-for fn in class1_files[6000:8000]:
-    img = io.imread(os.path.join(path_class_1, fn))
-    img = img.transpose((2, 0, 1))
-    img = img[:3, :, :]
-    X_test.append(img)
-    Y_test.append(1)
+for fn in class1_files[40:len(class1_files)]:
+	img = io.imread(os.path.join(path_class_1, fn))
+	img = img.transpose((2, 0, 1))
+	img = img[:3, :, :]
+	X_test.append(img)
+	Y_test.append(1)
 
 X_test = np.asarray(X_test)
 Y_test = np.asarray(Y_test)
@@ -164,41 +165,41 @@ Y_test = np_utils.to_categorical(Y_test, nb_classes)
 # In[15]:
 
 def make_model():
-    model = Sequential()
-    print(len(kernel_size))
-    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
-                            border_mode='valid',
-                            input_shape=input_shape))
-    model.add(Activation('relu'))
-    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=pool_size))
-    model.add(Dropout(0.25))
+	model = Sequential()
+	print(len(kernel_size))
+	model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
+							border_mode='valid',
+							input_shape=input_shape))
+	model.add(Activation('relu'))
+	model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1], dim_ordering="th"))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=pool_size))
+	model.add(Dropout(0.25))
 
-    model.add(Convolution2D(nb_filters, 3, 3, border_mode='same'))
-    model.add(Activation('relu'))
-    model.add(Convolution2D(nb_filters, 3, 3))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+	model.add(Convolution2D(nb_filters, 3, 3, border_mode='same'))
+	model.add(Activation('relu'))
+	model.add(Convolution2D(nb_filters, 3, 3))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.25))
 
-    model.add(Flatten())
-    model.add(Dense(128))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(nb_classes))
-    model.add(Activation('softmax'))
+	model.add(Flatten())
+	model.add(Dense(128))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.5))
+	model.add(Dense(nb_classes))
+	model.add(Activation('softmax'))
 
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adadelta',
-                  metrics=['accuracy'])
-    return model
+	model.compile(loss='binary_crossentropy',
+					optimizer='adadelta',
+					metrics=['accuracy'])
+	return model
 
 def load_image(filename):
-    img = io.imread(filename)
-    img = img.transpose((2, 0, 1))
-    img = img[:3, :, :]
-    return img
+	img = io.imread(filename)
+	img = img.transpose((2, 0, 1))
+	img = img[:3, :, :]
+	return img
 
 
 # In[16]:
@@ -210,10 +211,10 @@ model.compile(loss='binary_crossentropy', optimizer='adadelta', metrics=['accura
 # In[13]:
 
 for e in range(3):
-    model.fit(X_t, Y_t,
-              # validation_data=(X_test, Y_test),
-              batch_size=batch_size,
-              nb_epoch=1, verbose=1)
+	model.fit(X_t, Y_t,
+			# validation_data=(X_test, Y_test),
+			batch_size=batch_size,
+			nb_epoch=1, verbose=1)
 
 
 # ## Trying if all this stuff works
@@ -224,13 +225,13 @@ predictions = model.predict_classes(X_test)
 
 y = []
 for e in Y_test:
-    if e[0] > e[1]:
-        y.append(0)
-    else:
-        y.append(1)
+	if e[0] > e[1]:
+		y.append(0)
+	else:
+		y.append(1)
 
 print('how many did we guess out of ', Y_test.shape)
-np.sum(y == predictions)
+print(np.sum(y == predictions))
 
 
 # # Predictions on new audio sample
@@ -238,7 +239,7 @@ np.sum(y == predictions)
 # In[ ]:
 
 # the data, shuffled and split between train and test sets
-path_newsample = '/archive/ahem_data/new_sample/'
+path_newsample = './archive/ahem_data/new_sample/'
 newsample_files = [f for f in listdir(path_newsample) if isfile(join(path_newsample, f))]
 
 
@@ -248,10 +249,10 @@ newsample_files = [f for f in listdir(path_newsample) if isfile(join(path_newsam
 X_test = []
 
 for fn in newsample_files:
-    img = io.imread(os.path.join(path_newsample, fn))
-    img = img.transpose((2, 0, 1))
-    img = img[:3, :, :]
-    X_test.append(img)
+	img = io.imread(os.path.join(path_newsample, fn))
+	img = img.transpose((2, 0, 1))
+	img = img[:3, :, :]
+	X_test.append(img)
 
 X_test = np.asarray(X_test)
 X_test = X_test.astype('float32')
@@ -277,7 +278,7 @@ noisy_files = [newsample_files[n] for n in noisy_frames]
 # In[ ]:
 
 # Load a sound with a lot of "ahem" in it
-path = '/archive/ahem_data'
+path = './archive/ahem_data'
 sound_file_paths = [os.path.join(path, "provocation_dirty.wav")]
 sound_names = ["dirty"]
 raw_sounds = ut.load_sound_files(sound_file_paths)
@@ -296,14 +297,14 @@ clean_audio = audiosamples
 # In[ ]:
 
 # Playback from ipython (cool uh?)
-IPython.display.Audio(data=original_audio, rate=44100)
+# IPython.display.Audio(data=original_audio, rate=44100)
 
 
 # In[ ]:
 
 noisy_start = []
 for fn in noisy_files:
-    noisy_start.append(int(fn.split('_')[2].split('.')[0]))
+	noisy_start.append(int(fn.split('_')[2].split('.')[0]))
 
 noisy_start.sort(reverse=True)
 
@@ -313,25 +314,25 @@ noisy_start.sort(reverse=True)
 clean_audio = audiosamples
 prev_idx = 0
 for start in range(1, len(noisy_start)):
-    prev_pos = noisy_start[prev_idx]
-    current_pos = noisy_start[start]
-    diff = prev_pos - current_pos
-    prev_idx += 1
+	prev_pos = noisy_start[prev_idx]
+	current_pos = noisy_start[start]
+	diff = prev_pos - current_pos
+	prev_idx += 1
 
-    # set volume to zero for 'ahem' samples
-    clean_audio[current_pos:current_pos + windowsize] = 0
+	# set volume to zero for 'ahem' samples
+	clean_audio[current_pos:current_pos + windowsize] = 0
 
 
 # In[ ]:
 
 # Play it back!
-IPython.display.Audio(data=clean_audio, rate=44100)
+# IPython.display.Audio(data=clean_audio, rate=44100)
 
 
 # In[ ]:
 
 # save to file and enjoy the clean episode!
-librosa.output.write_wav('/archive/ahem_data/cleaned.wav', clean_audio, sr=44100)
+librosa.output.write_wav('./archive/ahem_data/cleaned.wav', clean_audio, sr=44100)
 
 
 # # Audio analytics without deep learning
@@ -344,7 +345,7 @@ ut.plot_log_power_specgram(sound_names, raw_sounds)
 
 # traditional audio features
 mfccs, chroma, mel, contrast, tonnetz = ut.extract_feature('./data/jingle.wav')
-ut.specgram_frombuffer(raw_sounds[0][0:44100], 6, 6, fname='/archive/buffer.png', show=True)
+ut.specgram_frombuffer(raw_sounds[0][0:44100], 6, 6, fname='./archive/buffer.png', show=True)
 
 # found a good model to analyze the audio features above
 # and... good luck!
